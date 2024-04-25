019c908e9cf0ef2b28964d320277938d7316acfa
Cristian-Mathias <cristian11mathias@hotmail.com>
2024-04-25 15:24:12 -0300

    atualizando endereço API

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
  async function fetchProfileData(){
-    const url = 'https://github.com/Cristian-Mathias/Criando-e-Estilizando-uma-Pagina-de-Portfolio-Profissional-com-CSS-e-JavaScript-DIO.me/blob/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/Cristian-Mathias/Criando-e-Estilizando-uma-Pagina-de-Portfolio-Profissional-com-CSS-e-JavaScript-DIO.me/main/data/profile.json'
     const response =await fetch(url)
     const profileData = await response.json()
     return profileData
--- assets/js/main.js
+++ assets/js/main.js
@@ -23,13 +23,11 @@ function updateProfileInfo(profileData) {
 
 function updateSoftSkills(profileData) {
     const softSkills = document.getElementById('profile.skills.softSkills')
-
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li> ${skill}</li>`).join('')
 }
 
 function updateHardSkills(profileData) {
     const hardSkills = document.getElementById('profile.skills.hardSkills')
-
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li> <img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
 
